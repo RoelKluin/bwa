b73be5926cb940ceff97c9eed5e23a39ce3e159d
Roel Kluin <roel.kluin@gmail.com>
2013-01-30 23:16:19 +0100

    alter error codes of kseq_read()
    Signed-off-by: Roel Kluin <roel.kluin@gmail.com>

--- kseq.h
+++ kseq.h
@@ -110,8 +110,9 @@ static int ks_getuntil(kstream_t *ks, int delimiter, kstring_t *str)
 		if (ks->begin >= ks->end) {
 			if (ks->end == KS_BUFSIZE) {
 				ks->buf -= ks->begin;
-				if ((ks->end = KS_READ(ks->f, ks->buf, KS_BUFSIZE)) <= 0) {
-					delimiter = ks->end == 0 ? -1 : -4;
+				ks->end = KS_READ(ks->f, ks->buf, KS_BUFSIZE);
+				if (ks->end <= 0) {
+					delimiter = ks->end - 1;
 					break;
 				}
 				ks->begin = 0;
@@ -131,7 +132,7 @@ static int ks_getuntil(kstream_t *ks, int delimiter, kstring_t *str)
 			str->m = str->l + i + 1;
 			kroundup32(str->m);
 			if ((str->s = (char*)realloc(str->s, str->m)) == NULL) {
-				delimiter = -3;
+				delimiter = -4;
 				break;
 			}
 		}
@@ -184,9 +185,9 @@ static inline void kseq_destroy(kseq_t *ks)
 /* Return value:
    >=0  length of the sequence (normal)
    -1   end-of-file
-   -2   truncated quality string
-   -3   allocation error
-   -4   instream read error (via ks_getuntil())
+   -2   instream read error (via ks_getuntil())
+   -3   truncated quality string
+   -4   allocation error
  */
 static int kseq_read(kseq_t *seq)
 {
@@ -221,22 +222,22 @@ static int kseq_read(kseq_t *seq)
 	}
 	if (c == '>' || c == '@') seq->last_char = c; /* the first header char has been read */
 	if (seq->seq.s == NULL)
-		return -3;
-	seq->seq.s[seq->seq.l] = 0;	/* null terminated string */
+		return -4;
+	seq->seq.s[seq->seq.l] = '\0';	/* null terminated string */
 	if (c != '+') return seq->seq.l; /* FASTA */
 	if (seq->qual.m < seq->seq.m) {	/* allocate enough memory */
 		seq->qual.m = seq->seq.m;
 		seq->qual.s = (char*)realloc(seq->qual.s, seq->qual.m);
 		if (seq->qual.s == NULL)
-			return -3;
+			return -4;
 	}
 	while ((c = ks_getc(ks)) != -1 && c != '\n'); /* skip the rest of '+' line */
-	if (c == -1) return -2; /* we should not stop here */
+	if (c == -1) return -3; /* we should not stop here */
 	while ((c = ks_getc(ks)) != -1 && seq->qual.l < seq->seq.l)
 		if (c >= 33 && c <= 127) seq->qual.s[seq->qual.l++] = (unsigned char)c;
 	seq->qual.s[seq->qual.l] = 0; /* null terminated string */
 	seq->last_char = 0;	/* we have not come to the next header line */
-	if (seq->seq.l != seq->qual.l) return -2; /* qual string is shorter than seq string */
+	if (seq->seq.l != seq->qual.l) return -3; /* qual string is shorter than seq string */
 	return seq->seq.l;
 }
 
